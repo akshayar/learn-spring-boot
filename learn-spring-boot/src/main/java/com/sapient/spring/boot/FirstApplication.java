e9b3c79b61cc2529ca515479c7bab7bc2693a31f
Akshaya Rawat <akshaya.rawat@gmail.com>
2015-12-05 19:10:58 +0530

    First draft with one class as both application and conroller.

---
+++ learn-spring-boot/src/main/java/com/sapient/spring/boot/FirstApplication.java
@@ -0,0 +1,29 @@
+package com.sapient.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sapient.spring.boot.dto.Trade;
+
+@EnableAutoConfiguration
+@RestController
+public class FirstApplication {
+
+	@RequestMapping("ping")
+	public String ping(){
+		return "OK";
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,path="trade")
+	private Trade create(){
+		return new Trade();
+		
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+	}
+}
