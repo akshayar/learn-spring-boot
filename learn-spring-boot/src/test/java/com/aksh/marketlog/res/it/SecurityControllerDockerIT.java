b31b6b1fb551d4788542917820fb2935e3fe6c9b
Akshaya Rawat <akshaya.rawat@gmail.com>
2017-02-24 22:48:00 -0800

    Added docker integration testing example

--- learn-spring-boot/src/test/java/com/aksh/marketlog/res/it/SecurityControllerDockerIT.java
+++ learn-spring-boot/src/test/java/com/aksh/marketlog/res/it/SecurityControllerDockerIT.java
@@ -2,6 +2,7 @@ package com.aksh.marketlog.res.it;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,14 @@ public class SecurityControllerDockerIT {
 
 	private RestTemplate restTemplate=new RestTemplate();
 	private String serverUrl="http://localhost:8080";
+	
+	@Before
+	public void setup(){
+		System.out.println(System.getenv());
+		System.out.println(System.getProperties());
+		serverUrl="http://"+System.getProperty("docker.host.address")+":8080";
+		System.out.println("In unit test server url:"+serverUrl);
+	}
 
 	@Test
 	public void pingtest() {
