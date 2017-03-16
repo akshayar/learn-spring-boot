711637c4aaedf67283a9717711e91ae39085839d
Akshaya Rawat <akshaya.rawat@gmail.com>
2017-03-15 22:41:31 -0700

    Added logs

--- src/main/java/com/sapient/spring/boot/web/SecurityController.java
+++ src/main/java/com/sapient/spring/boot/web/SecurityController.java
@@ -3,6 +3,9 @@
  */
 package com.sapient.spring.boot.web;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,11 +20,14 @@ import com.sapient.spring.boot.dto.Security;
 @RestController
 public class SecurityController {
 	
+	private static final Logger logger=Logger.getLogger(SecurityController.class);
+	
 	@Value("${project.name}")
 	private String name;
 
 	@RequestMapping(path="security/ping")
-	public String ping(){
+	public String ping(HttpServletRequest request){
+		logger.info("Pinged from "+request.getRemoteAddr());
 		return "OK "+name;
 	}
 	
